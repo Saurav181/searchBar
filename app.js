d8099d2b2f36246075ede2c71ffd140a7ebefa41
Saurav181 <63736241+Saurav181@users.noreply.github.com>
2020-07-31 00:16:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ let hpCharacters = [];
 
 onsubmit = (event) => {
     event.preventDefault()
-    fetch("https://the-cocktail-db.p.rapidapi.com/list.php?a=list", {
+    fetch("", {
         "method": "GET",
         "headers": {
-            "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
-            "x-rapidapi-key": "b0fd893eb7msh2a3a5445b6e278cp19e907jsne4ff857b4414"
+            "x-rapidapi-host": "",
+            "x-rapidapi-key": ""
         }
     })
         .then(response => {
